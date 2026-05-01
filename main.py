79725fc89632a5f6b2157514dfdbbcaf877dd8bd
Velvetvibes69 <amayasunil11@gmail.com>
2026-05-01 19:34:53 +1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#Print a Scentence
+print("wlcome to the world of programing")
+
+#Print a Number
+print(4)
+
+#Next line after print
+print("Hello Python /n")
+
+#Pass multiple argument for printing
+print("Hello", 5)
+
+#End argument for print statement
+print("Wlcome to", end ="*")
